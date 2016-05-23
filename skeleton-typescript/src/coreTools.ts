9f26ff2836eae6306ea943fc8cefe7e634ff979e
Zachary VanderLaan <zsvanderlaan@gmail.com>
2016-05-22 17:35:29 -0700

    initial commit
    (cherry picked from commit 2301ae68e9d282456ea466de79e6b079bb9661f4)

--- skeleton-typescript/src/coreTools.ts
+++ skeleton-typescript/src/coreTools.ts
@@ -77,6 +77,28 @@ export function autoconfigure(potentialTarget?: any): any {
   // We can accomplish this by using typescript reflection, as is done in other aurelia decorators
 }
 
+export function log(target: Function, key: string, descriptor: any) {
+
+  // save a reference to the original method
+  // this way we keep the values currently in the
+  // descriptor and don't overwrite what another
+  // decorator might have done to the descriptor.
+  var originalMethod = descriptor.value;
+
+  //editing the descriptor/value parameter
+  descriptor.value =  function (...args: any[]) {
+    var a = args.map(a => JSON.stringify(a)).join();
+    // note usage of originalMethod here
+    var result = originalMethod.apply(this, args);
+    var r = JSON.stringify(result);
+    console.log(`Call: ${key}(${a}) => ${r}`);
+    return result;
+  }
+
+  // return edited descriptor as opposed to overwriting
+  // the descriptor by returning a new descriptor
+  return descriptor;
+}
 
 // export function clone(): any {
 //   var cloneObj = new (<any>this.constructor)(); // line fixed
